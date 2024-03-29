2844bbac3132c68d2a5420b78e8608353d525913
oathkite <kita@impv.co.jp>
2024-03-29 18:47:14 +0900

    feat: NewtApiClientを実装

--- src/api/newtApClient.ts
+++ src/api/newtApClient.ts
@@ -1,1 +1,7 @@
 import { createClient } from 'newt-client-js';
+
+export const newtApClient = (isPreview: boolean) => createClient({
+  spaceUid: import.meta.env.NEWT_SPACE_UID,
+  token: isPreview ? import.meta.env.NEWT_API_TOKEN : import.meta.env.NEWT_PREVIEW_API_TOKEN,
+  apiType: isPreview ? "api" : "cdn",
+});
