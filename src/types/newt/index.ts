08e04eec4ace93f26ec54dace14fa9b042b501dc
oathkite <kita@impv.co.jp>
2024-04-05 18:36:38 +0900

    feat: About実装

--- src/types/newt/index.ts
+++ src/types/newt/index.ts
@@ -1,5 +1,4 @@
 export type { Domain } from './domain';
-export type { File } from './file';
 export type { Member } from './member';
 export type { Partner } from './partner';
 export type { Client } from './client';
