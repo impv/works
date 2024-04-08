76e7e6d7cf1ba136558d35e0d11f966dcadcea9a
oathkite <kita@impv.co.jp>
2024-04-09 00:52:00 +0900

    feat: ブラッシュアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react()],
+  devToolbar: {
+    enabled: false,
+  },
 });
