6191b9d25e6e1b38ecd8fc789d749d027b8aaf51
oathkite <kita@impv.co.jp>
2024-05-02 14:46:52 +0900

    ogImageの処理を削除

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import { createOGImage } from "./src/integrations/ogimage/index.tsx";
 
 export default defineConfig({
   output: "hybrid",
   integrations: [
     mdx(),
     react(),
-    createOGImage({
-      config: {
-        path: "/culture",
-      },
-    })
   ],
   prefetch: true,
   devToolbar: {
